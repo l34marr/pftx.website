ca8592e3efa5b7cad90fe5510bfbeabb75d1cbd7
TsungWei Hu <marr.tw@gmail.com>
2018-11-17 08:19:22 +0000

    Add QRCodeView

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Products.GenericSetup>=1.8.2',
         'setuptools',
         'z3c.jbot',
+        'qrcode',
     ],
     extras_require={
         'test': [
--- src/pftx/website/browser/view.py
+++ src/pftx/website/browser/view.py
@@ -3,6 +3,8 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
+from cStringIO import StringIO
+import qrcode
 
 
 class MyView(BrowserView):
@@ -35,4 +37,30 @@ class SuYuanView(BrowserView):
     def hasValue(self, mid):
         mmbr = self.mmbr(mid)
         return True and (mmbr['home_page'] or mmbr['report'] or mmbr['panorama']) or False
+
+
+class QRCodeView(BrowserView):
+
+    def __call__(self):
+        qr = qrcode.QRCode(
+            version = 1,
+            error_correction = qrcode.constants.ERROR_CORRECT_H,
+            box_size = 10,
+            border = 4,
+        )
+
+        url = self.context.absolute_url()
+
+        qr.add_data(url)
+        qr.make(fit=True)
+
+        # Create an Image from the QR Code Instance
+        img = qr.make_image()
+
+        # Save it Somewhere, Change the Extension as Needed:
+        img.save("image.png")
+        output = StringIO()
+        img.save(output)
+        self.request.response.setHeader('Content-Type', 'image/png')
+        return output.getvalue()
 
